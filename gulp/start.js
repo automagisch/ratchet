137f493a8f135ea1b16454478b1b50f9e9c1cb87
Koen Houtman <koenhoutman1991@me.com>
2017-01-23 10:53:39 +0100

    display a neat ascii message on startup!

--- gulp/start.js
+++ gulp/start.js
@@ -1,12 +1,43 @@
+var fs 		= require('fs');
+var chalk 	= require('chalk');
 var gulp 	= require('gulp');
 var nodemon = require('gulp-nodemon');
+var config 	= require('ini').parse(fs.readFileSync('./config.ini', 'utf-8'));
+
+function displayASCIIArt() {
+
+	console.log(chalk.cyan(
+	` 	
+
+	~~~~~~~~~~~~~ https://github.com/automagisch/ratchet ~~~~~~~~~~~~~~~
+
+	______    _______  _______  _______  __   __  _______  _______ 
+	|    _ |  |   _   ||       ||       ||  | |  ||       ||       |
+	|   | ||  |  |_|  ||_     _||       ||  |_|  ||    ___||_     _|
+	|   |_||_ |       |  |   |  |       ||       ||   |___   |   |  
+	|    __  ||       |  |   |  |      _||       ||    ___|  |   |  
+	|   |  | ||   _   |  |   |  |     |_ |   _   ||   |___   |   |  
+	|___|  |_||__| |__|  |___|  |_______||__| |__||_______|  |___|  1.0
+
+	F R O N T - E N D 
+	B O I L E R P L A T E
+
+	~~~~~~~~~~~~~~~~~~~~~~~ (c) Koen Houtman, 2017 ~~~~~~~~~~~~~~~~~~~~~
+
+	`));
+
+}
+
+if(config.ratchet.show_ascii_art) {
+	displayASCIIArt();
+}
 
 /**
  * START task
  * @description: boots up the server simultaneously with gulp
  */
 module.exports = function() {
-	
+
 	// boot up nodeman, only watching the root folder with exceptions to keep restarting to a minimum
 	nodemon({
 		ext: 'js',
