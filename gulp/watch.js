68a1f921a03029e3d81a743a66111d59433d8aae
Koen Houtman <koenhoutman1991@me.com>
2017-02-13 12:53:34 +0100

    Fix in watcher for new files in src/images
    - also made 1 image file for ratchet logo (@2x optimized)

--- gulp/watch.js
+++ gulp/watch.js
@@ -8,14 +8,21 @@ module.exports = function() {
 
 	// watches the scss folder for changes, if a change is detected, it runs task 'styles'
 	gulp.watch('src/scss/**/*.scss', ['styles']);
+
 	// watches the html folder for changes, if a change is detected, it runs task 'html'
 	gulp.watch('src/html/**/*.html', ['html']);
+
 	// watches the scripts folder for changes, if a change is detected, it runs task 'scripts'
+	// (if use_es6 in config is false, this task is used for compiling javascript)
 	gulp.watch('src/scripts/**/*.js', ['scripts']);
+
 	// watches the scripts folder for changes, if a change is detected, it runs task 'babel'
+	// (if use_es6 in config is true, this task is used for compiling javascript)
 	gulp.watch('src/scripts/**/*.babel', ['babel']);
+
 	// watches the images folder for changes, if a change is detected, it runs task 'images'
-	gulp.watch('src/images/**/*.{jpg|png|svg}', ['images']);
+	gulp.watch(['src/images/**/*.{jpg|png|svg}','src/images/**/*'], ['images']);
+
 	// watches the handlebars folder for changes, if a change is detected, it runs task 'handlebars'
 	gulp.watch('src/handlebars/**/*.hbs', ['handlebars']);
 
