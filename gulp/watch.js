4a6664a8b4296219f7e4288dfda2abcd7cef991d
Koen Houtman <koenhoutman1991@me.com>
2017-02-27 18:47:10 +0100

    Implemented gulp-watch
    - the standard gulp watcher is apperently not so good, so I started using gulp-watch, as it exposes events!
    - implemented utility function for deleting files from the distribution directory. If you would remove a file from /src/images, they will now also remove from /static/assets/images

--- gulp/watch.js
+++ gulp/watch.js
@@ -1,4 +1,25 @@
-var gulp = require('gulp');
+var fs    = require('fs');
+var gulp  = require('gulp');
+var path  = require('path');
+var watch = require('gulp-watch');
+var chalk = require('chalk');
+
+/*
+	Utility function for deleting files to avoid
+	duplicates and 'unlinked' files from src to dist
+ */
+function syncDeletion(p, callback) {
+
+	if(!callback) var callback = function(){};
+
+	var srcFile = path.parse(p);
+	var distPath = srcFile.dir.replace('src/','dist/assets/');
+	if(fs.existsSync(distPath + '/' + srcFile.base)) {
+		fs.unlink(distPath + '/' + srcFile.base, function() {
+			callback(srcFile.base);
+		});
+	}
+}
 
 /**
  * WATCH task
@@ -7,24 +28,28 @@ var gulp = require('gulp');
 module.exports = function(done) {
 
 	// watches the scss folder for changes, if a change is detected, it runs task 'styles'
-	gulp.watch('src/scss/**/*.scss', gulp.series(['styles']));
+	watch('src/scss/**/*.scss', gulp.series(['styles']));
 
 	// watches the html folder for changes, if a change is detected, it runs task 'html'
-	gulp.watch('src/html/**/*.html', gulp.series(['html']));
+	watch('src/html/**/*.html', gulp.series(['html']));
 
 	// watches the scripts folder for changes, if a change is detected, it runs task 'scripts'
 	// (if use_es6 in config is false, this task is used for compiling javascript)
-	gulp.watch('src/scripts/**/*.js', gulp.series(['scripts']));
+	watch('src/scripts/**/*.js', gulp.series(['scripts']));
 
 	// watches the scripts folder for changes, if a change is detected, it runs task 'babel'
 	// (if use_es6 in config is true, this task is used for compiling javascript)
-	gulp.watch('src/scripts/**/*.babel', gulp.series(['babel']));
+	watch('src/scripts/**/*.babel', gulp.series(['babel']));
 
 	// watches the images folder for changes, if a change is detected, it runs task 'images'
-	gulp.watch('src/images/**/*.{jpg|png|svg}', gulp.series(['images']));
+	watch('src/images/**/*.{jpg|png|svg}', gulp.series(['images'])).on('unlink', function(file) {
+		syncDeletion(file, function(removedFile) {
+			console.log(chalk.red('Ratchet: deleted ' + removedFile + ' from distribution folder.'));
+		});
+	});
 
 	// watches the handlebars folder for changes, if a change is detected, it runs task 'handlebars'
-	gulp.watch('src/handlebars/**/*.hbs', gulp.series(['handlebars']));
+	watch('src/handlebars/**/*.hbs', gulp.series(['handlebars']));
 
 	// opt out this gulp task
 	done();
