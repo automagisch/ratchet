39fbed14cbe55131c0deb5481a4a369276ad7f98
Koen Houtman <koenhoutman1991@me.com>
2017-03-17 14:56:22 +0100

    babel errors more humane and with respect

--- gulp/babel.js
+++ gulp/babel.js
@@ -1,9 +1,11 @@
+var path = require('path');
 var fs = require('fs');
 var gulp = require('gulp');
 var browserify = require('browserify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
+var chalk = require('chalk');
 
 module.exports = function(done) {
 
@@ -18,7 +20,12 @@ module.exports = function(done) {
 			sourceRoot: './src/scripts/'
 		})
 		.bundle()
-		.on('error', function(err) { console.log(err); })
+		.on('error', function(err) {
+			console.log(chalk.red('\nOopsie-daysee! You made an unforgivable typo:\n'));
+			console.log(`${err.codeFrame}\n`);
+			console.log(`>>> ${path.basename(err.fileName)} (line ${err.loc.line}, col ${err.loc.column} ]\n`);
+			this.emit('end');
+		})
 		.pipe(source('main.js'))
 		.pipe(buffer())
 		.pipe(gulp.dest('./dist/assets/js'));
