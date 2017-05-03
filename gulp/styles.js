cc553466a02e06aeb7a53d69e96e269f4cb5ab6b
Koen Houtman <koenhoutman1991@me.com>
2017-05-03 11:12:19 +0200

    Added css minifier to the stack
    - icw gulp-util for passing env arguments, one is now able to 'gulp styles --production' to minify their css upon building.

--- gulp/styles.js
+++ gulp/styles.js
@@ -1,6 +1,9 @@
-var gulp 		= require('gulp');
-var sass 		= require('gulp-ruby-sass');
+var gulp 				= require('gulp');
+var gutil 			= require('gulp-util');
+var sass 				= require('gulp-ruby-sass');
 var sourcemaps 	= require('gulp-sourcemaps');
+var minify 			= require('gulp-clean-css');
+var converter 	= require('byte-converter').converterBase2;
 
 /**
  * STYLES task
@@ -10,7 +13,7 @@ module.exports = function(done) {
 
 	// initiates main.scss compilation in src folder
 	return sass('./src/scss/main.scss', {
-		sourcemap: true,
+		sourcemap: gutil.env.production ? false : true,
 		loadPath: [
 			'bower_components/font-awesome/scss'
 		]
@@ -20,10 +23,20 @@ module.exports = function(done) {
 		.on('error', sass.logError)
 
 		// generates a file source map
-		.pipe(sourcemaps.write({
+		.pipe(gutil.env.production ? gutil.noop() : sourcemaps.write({
 	      includeContent: false,
 	      sourceRoot: 'source'
 	    }))
+
+		// in production mode, minify the output
+		.pipe(gutil.env.production ? minify({compatibility: 'ie8'}, function(details) {
+
+			var originalSize = Math.round(converter(details.stats.originalSize, 'B', 'KB') * 10) / 10;
+			var compressedSize = Math.round(converter(details.stats.minifiedSize, 'B', 'KB') * 10) / 10;
+
+			gutil.log(gutil.colors.green(`CSS minified, compressed from: ${originalSize}kb to ${compressedSize}kb.`));
+
+		}) : gutil.noop())
 
 	    // output to dist asset folder
 		.pipe(gulp.dest('dist/assets/css'));
